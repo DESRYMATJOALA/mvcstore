861aaa2f407dba42a011a04b0597e1f5b8ffa9d5
Firstname Lastname <your.email@example.org>
2021-01-21 00:39:00 -0800

    first commit

---
+++ mvcstore/src/main/java/com/micro/mvcstore/MvcstoreApplication.java
@@ -0,0 +1,13 @@
+package com.micro.mvcstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcstoreApplication.class, args);
+	}
+
+}
---
+++ mvcstore/src/test/java/com/micro/mvcstore/MvcstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.mvcstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
